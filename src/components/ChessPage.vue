f1b68df6a4aaff970d58842ed30eaab14b2e854c
LBranco27 <lucasbranco28@protonmail.com>
2022-09-21 16:56:34 -0300

    passaro mensageiro adicionado

--- src/components/ChessPage.vue
+++ src/components/ChessPage.vue
@@ -45,10 +45,15 @@ export default {
 	<div id="app">
 	<table id="table1" cellspacing="2" cellpadding="2" align="center" border="5" bgcolor="darkblue" bordercolor="#ffc900">
 		<tbody>
-			<tr>
+			<tr bgcolor="black">
+				<td>
 				<div align="center">
-					<img src="img/gnome.gif"/>
+					<img src="img/chessmaster.png" width="250"/>
 				</div>
+				</td>
+				<td>
+					
+				</td>
 			</tr>
 			<tr>
 				<td>
@@ -66,17 +71,22 @@ export default {
 						</marquee>	
 					</div>
 					<div>
-						asd
+						
 					</div>
 				</td>
 			</tr>
 		</tbody>
 	</table>
+	<div id="letter1" >
+		<marquee direction="left" loop="1" scrollamount="15" style="position:fixed;bottom: 100px;"><img src="img/seagull_with_letter_left.gif" /></marquee>
+	</div>
+	<div id="letter2"></div>
+
+
+
 	<div >
 		<img style="position:fixed;bottom:90px;" src="/img/gnome.gif" ref="gnome" @click="gnome" title="You've found a gnome!!! Click on it!"/>
 		<audio ref="gaudio" src="/snd/gnome-woo.mp3"></audio>
-	</div>
-	<div>
 	</div>
 	</div>
 </template>
